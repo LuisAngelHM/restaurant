05dec6e842918b4a7494fc190b7306196442327f
Luis Angel Herrera Maldonado <luismaldonado25@outlook.com>
2023-03-08 16:12:56 -0600

    Add views for  menu and booking sections

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,30 +1,16 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-# Create your views here.
-def index(request, day):
-    menu = {
-        "lunes": ["Sopal", "Carnel", "Verdurasl"],
-        "martes": ["Sopam", "Carnem", "Verdurasm"],
-        "miercoles": ["Sopam", "Carnem", "Verdurasm"],
-        "jueves": ["Sopaj", "Carnej", "Verdurasj"],
-        "viernes": ["Sopav", "Carnev", "Verdurasv"],
-    }
-    try: 
-        response = {"menu": menu[day]}
-        return JsonResponse(response)
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import menuSerializer, bookingSerializer
 
-    except Exception as error: 
-        print(error)
-        response = { "message": error, "code": 500}
-        return JsonResponse(response)
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
 
-def menuEspecial(request): 
-    menuEspecial = {
-        "LunesEsp": ["Sopa", "Carne", "Verduras"],
-        "MartesEsp": ["Sopa", "Carne", "Verduras"],
-        "MiercolesEsp": ["Sopa", "Carne", "Verduras"],
-        "JuevesEsp": ["Sopa", "Carne", "Verduras"],
-        "ViernesEsp": ["Sopa", "Carne", "Verduras"],
-    }
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
 
-    return JsonResponse(menuEspecial)
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
